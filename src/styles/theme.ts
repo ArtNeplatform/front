8c7808f79ff137280bf5ce54ee98236197056382
kyujenius <hkj020607@gmail.com>
2025-01-31 23:12:42 +0900

    Fix: fontGray 및 priceBox 타입 추가

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -25,39 +25,39 @@ type Typography = {
 // 테마 정의
 const theme = {
   typography: {
-    56: { fontSize: "56px", lineHeight: "72px", letterSpacing: "-2.5%" },
-    48: { fontSize: "48px", lineHeight: "62px", letterSpacing: "-2.5%" },
-    44: { fontSize: "44px", lineHeight: "56px", letterSpacing: "-2.5%" },
-    38: { fontSize: "38px", lineHeight: "50px", letterSpacing: "-2.5%" },
-    32: { fontSize: "32px", lineHeight: "42px", letterSpacing: "-2.5%" },
-    28: { fontSize: "28px", lineHeight: "38px", letterSpacing: "-2.5%" },
-    24: { fontSize: "24px", lineHeight: "34px", letterSpacing: "-2.5%" },
-    20: { fontSize: "20px", lineHeight: "30px", letterSpacing: "-2.5%" },
-    18: { fontSize: "18px", lineHeight: "28px", letterSpacing: "-2.5%" },
-    16: { fontSize: "16px", lineHeight: "26px", letterSpacing: "-2.5%" },
-    15: { fontSize: "15px", lineHeight: "22px", letterSpacing: "-2.5%" },
-    14: { fontSize: "14px", lineHeight: "20px", letterSpacing: "-2.5%" },
-    13: { fontSize: "13px", lineHeight: "18px", letterSpacing: "-2.5%" },
+    56: { fontSize: '56px', lineHeight: '72px', letterSpacing: '-2.5%' },
+    48: { fontSize: '48px', lineHeight: '62px', letterSpacing: '-2.5%' },
+    44: { fontSize: '44px', lineHeight: '56px', letterSpacing: '-2.5%' },
+    38: { fontSize: '38px', lineHeight: '50px', letterSpacing: '-2.5%' },
+    32: { fontSize: '32px', lineHeight: '42px', letterSpacing: '-2.5%' },
+    28: { fontSize: '28px', lineHeight: '38px', letterSpacing: '-2.5%' },
+    24: { fontSize: '24px', lineHeight: '34px', letterSpacing: '-2.5%' },
+    20: { fontSize: '20px', lineHeight: '30px', letterSpacing: '-2.5%' },
+    18: { fontSize: '18px', lineHeight: '28px', letterSpacing: '-2.5%' },
+    16: { fontSize: '16px', lineHeight: '26px', letterSpacing: '-2.5%' },
+    15: { fontSize: '15px', lineHeight: '22px', letterSpacing: '-2.5%' },
+    14: { fontSize: '14px', lineHeight: '20px', letterSpacing: '-2.5%' },
+    13: { fontSize: '13px', lineHeight: '18px', letterSpacing: '-2.5%' },
   },
   colors: {
-    black: "#111",
-    gray: "#787878",
-    lightGray: "#E5E5EC",
-    white: "#FFFFFF",
-    dark: "#191a20",
-    primary: "#3f4150",
-    secondary: "#8c8d96",
-    lineLightColor: "#F1F1F5",
-    font03gray: "#505050",
-    fontGray: "#767676",
-    priceBox: "#F7F7FB",
+    black: '#111',
+    gray: '#787878',
+    lightGray: '#E5E5EC',
+    white: '#FFFFFF',
+    dark: '#191a20',
+    primary: '#3f4150',
+    secondary: '#8c8d96',
+    lineLightColor: '#F1F1F5',
+    font03gray: '#505050',
+    fontGray: '#767676',
+    priceBox: '#F7F7FB',
   },
 } as const;
 
 export type Theme = typeof theme;
 
 // emotion의 DefaultTheme 확장
-declare module "@emotion/react" {
+declare module '@emotion/react' {
   export interface Theme {
     typography: Typography;
     colors: {
@@ -70,6 +70,8 @@ declare module "@emotion/react" {
       secondary: string;
       lineLightColor: string;
       font03gray: string;
+      fontGray: string;
+      priceBox: string;
     };
   }
 }
