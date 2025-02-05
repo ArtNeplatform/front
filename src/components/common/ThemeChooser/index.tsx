38f697528c168ed476f510be389e48ab7b37a07f
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-02-05 16:55:14 +0900

    Refactor: 테마 props 전달

--- src/components/common/ThemeChooser/index.tsx
+++ src/components/common/ThemeChooser/index.tsx
@@ -1,16 +1,23 @@
 import { useState } from 'react';
 import { ThemeButton, Wrapper } from './index.style';
 
-const themes = ['풍경', '인물', '정물', '동물', '추상', '팝아트', '오브제'];
+interface ThemeChooserProps {
+  themes: string[];
+  defaultSelectedTheme?: string;
+}
 
 /**
- * 테마 선택 컴포넌트입니다.
- * 작품의 테마를 선택할 수 있고 기본값은 '풍경' 입니다.
+ * 테마 선택 공통 컴포넌트입니다. 기본 선택 테마는 '풍경' 입니다.
+ * `themes`를 props로 받아 다른 페이지에서도 활용 가능하도록 작성하였습니다.
  * @author 노찬영
  */
 
-export const ThemeChooser = () => {
-  const [selectedTheme, setSelectedTheme] = useState<string>('풍경');
+export const ThemeChooser = ({
+  themes = ['풍경', '인물', '정물', '동물', '추상', '팝아트', '오브제'],
+  defaultSelectedTheme = '풍경',
+}: ThemeChooserProps) => {
+  const [selectedTheme, setSelectedTheme] =
+    useState<string>(defaultSelectedTheme);
 
   const handleThemeClick = (theme: string) => {
     setSelectedTheme(theme);
@@ -30,3 +37,5 @@ export const ThemeChooser = () => {
     </Wrapper>
   );
 };
+
+export default ThemeChooser;
