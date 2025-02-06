b0532156c2b85c2a0479d62f5d85cfae98ac0b88
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-02-07 01:01:40 +0900

    Refactor: 공통 컴포넌트 확장성 고려

--- src/components/common/ThemeChooser/index.tsx
+++ src/components/common/ThemeChooser/index.tsx
@@ -10,7 +10,7 @@ interface ThemeChooserProps {
  * 테마 선택 공통 컴포넌트입니다. 기본 선택 테마는 '풍경' 입니다.
  * `themes`를 props로 받아 다른 페이지에서도 활용 가능하도록 작성하였습니다.
  * @author 노찬영
- */
+ **/
 
 export const ThemeChooser = ({
   themes = ['풍경', '인물', '정물', '동물', '추상', '팝아트', '오브제'],
