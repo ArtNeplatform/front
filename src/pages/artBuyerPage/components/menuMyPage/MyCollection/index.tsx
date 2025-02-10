1cfdcaa62bbbd124b49c452271d0d9af6202f525
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-02-11 01:00:42 +0900

    Fix: 공통 컴포넌트 활용

--- src/pages/artBuyerPage/components/menuMyPage/MyCollection/index.tsx
+++ src/pages/artBuyerPage/components/menuMyPage/MyCollection/index.tsx
@@ -1,37 +1,18 @@
-import { useState } from 'react';
-
-import HeartIcon from '@assets/svg/Icon_heart.svg?react';
 import {
   MyCollectionContainer,
   SectionTitle,
   ArtworkGrid,
-  ArtworkItem,
-  ArtworkImage,
   ExhibitionGrid,
   ExhibitionItem,
   ExhibitionImage,
   ArtworkContainer,
   ExhibitionContainer,
-  ArtistName,
-  ArtworkName,
-  ArtworkImageWrapper,
-  ArtworkOverlay,
-  LikeButton,
 } from './index.style';
-import theme from '@/styles/theme';
 
 import { myCollection } from '@/pages/artBuyerPage/constants/myCollection';
+import { Artwork } from '@/components/common/ArtWork';
 
 const MyCollection = () => {
-  const [likedItems, setLikedItems] = useState<Record<number, boolean>>({});
-
-  const toggleLike = (id: number) => {
-    setLikedItems((prev) => ({
-      ...prev,
-      [id]: !prev[id],
-    }));
-  };
-
   return (
     <MyCollectionContainer>
       <h1>마이컬렉션</h1>
@@ -40,23 +21,14 @@ const MyCollection = () => {
         <SectionTitle>작품</SectionTitle>
         <ArtworkGrid>
           {myCollection.artworks.map((artwork) => (
-            <ArtworkItem key={artwork.id}>
-              <ArtworkImageWrapper>
-                <ArtworkImage src={artwork.image_url} alt={artwork.title} />
-                <ArtworkOverlay className="overlay">
-                  <LikeButton onClick={() => toggleLike(artwork.id)}>
-                    <HeartIcon
-                      fill={
-                        likedItems[artwork.id] ? theme.colors.white : 'none'
-                      }
-                    />
-                  </LikeButton>
-                </ArtworkOverlay>
-              </ArtworkImageWrapper>
-              <ArtistName>{artwork.author.name}</ArtistName>
-              <ArtworkName>{artwork.title}</ArtworkName>
-              <div>{artwork.size}</div>
-            </ArtworkItem>
+            <Artwork
+              key={artwork.id}
+              imageUrl={artwork.image_url}
+              title={artwork.title}
+              artist={artwork.artist.name}
+              artworkWidth={artwork.width}
+              artworkHeight={artwork.height}
+            />
           ))}
         </ArtworkGrid>
       </ArtworkContainer>
