3cc24a3ccb690ba4f62b0550254af5c41b8f9c50
twosky0202 <leehaneul0202@naver.com>
2025-02-12 05:52:57 +0900

    Feat: 전시 아이템 공통 컴포넌트 UI 구현

---
+++ src/components/common/ExhibitionItem/index.style.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const ArtworkItem = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ArtworkImage = styled.img`
+  width: 788px;
+  height: 630px;
+`;
+
+export const Title = styled.p`
+  margin-top: 20px;
+  font-weight: bold;
+`;
